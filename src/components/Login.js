854058b8083ba1654532d0d1216c165d09b3c333
kisu18 <141808648+kisu18@users.noreply.github.com>
2025-08-02 18:07:59 +0530

    Add files via upload

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,31 +1,34 @@
-import React, { useRef } from "react"
+import React, { useState } from "react"
 
 
 const Login = (props) => {
+  const [email,setEmail]=useState();
+  const [name,setName]=useState();
+
+  const loginhandler=(e)=>{
+      e.preventDefault();
+      props.onlogin();
+      props.onnamechange(name);
+      alert(`Logged in as ${name}`);
+        }
    
-    const name=useRef();
-    const loginhandler=(e)=>{
-        e.preventDefault();
-        props.onlogin();
-        props.onnamechange(name.current.value);
-        alert('Logged in');
-         }
-    
-    
-    return (
+     return (
       <div>
       <div class="login-container">
       <h2>Login to Bugflows</h2>
       <form id="loginForm" onSubmit={loginhandler}>
-        <input ref={name} type="text" placeholder="Name"   required />
-        <input type="email" placeholder="Email"  required />
+        <input  type="text" placeholder="Name" value={name} onChange={(e)=>setName(e.target.value)}  required />
+        <input type="email" placeholder="Email" value={email} onChange={(e)=>setEmail(e.target.value)} required />
         <input type="password" placeholder="Password" required />
         <button type="submit" >Login</button>
       </form>
       </div>
       </div>
     )
-};
+  }; 
+    
+  
+
 
 export default Login;
  
